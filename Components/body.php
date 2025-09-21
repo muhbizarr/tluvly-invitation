4cd23a5b7cda194e7eb09fa9d676b28404d28a01
muhbizarr <134249006+muhbizarr@users.noreply.github.com>
2025-09-21 19:12:03 +0700

    Add files via upload

---
+++ Components/body.php
@@ -0,0 +1,21 @@
+<body>
+    <section id="sectionKecil">
+      <div class="cover" id="cover">
+        <img
+          src="Assets/images/Bunga-awal.png"
+          alt="pengantin"
+          class="pengantin"
+        />
+        <p class="gowunbatang-bold">Undangan Pernikahan</p>
+        <h1>Marta & Rian</h1>
+        <div class="gowunbatang-bold">
+          Yang Terhormat <br />
+          Bapak/Ibu/Saudara/I
+        </div>
+        <button class="open gowunbatang-bold" id="open">
+          <img src="Assets/icon/envelope-paper-heart.svg" alt="Icon" />
+          Buka Undangan
+        </button>
+      </div>
+    </section>
+  </body>
---
+++ Components/body_core.php
@@ -0,0 +1,264 @@
+<body>
+    <audio id="myAudio" autoplay loop>
+      <source src="Assets/music/Westlife_-_Beautiful_In_White.mp3" type="audio/mpeg" />
+    </audio>
+    <button id="playButton" class="moon-button"></button>
+    <section id="sectionBesar">
+      <div class="pembungkus">
+        <p>THE WEDDING OF</p>
+        <h1>Marta & Rian</h1>
+        <p>28 . 09 . 2025</p>
+      </div>
+    </section>
+    <section id="sectionKecil">
+      <div class="cover" id="cover">
+        <img
+          src="Assets/images/Bunga-awal.png"
+          alt="pengantin"
+          class="pengantin"
+        />
+        <p class="gowunbatang-bold">The Wedding Of</p>
+        <h1>Marta & Rian</h1>
+        <p class="gowunbatang-bold">28 . 09 . 2025</p>
+      </div>
+      <div class="ayat">
+        <p class="gowunbatang-bold judul appear">Surah Ar-Rum Ayat 21</p>
+        <div class="arab appear">
+          <p>
+            وَمِنْ آيَاتِهِ أَنْ خَلَقَ لَكُمْ مِنْ أَنْفُسِكُمْ أَزْوَاجًا
+            لِتَسْكُنُوا إِلَيْهَا وَجَعَلَ بَيْنَكُمْ مَوَدَّةً وَرَحْمَةً ۚ
+            إِنَّ فِي ذَٰلِكَ لَآيَاتٍ لِقَوْمٍ يَتَفَكَّرُونَ
+          </p>
+        </div>
+        <div class="translation appear">
+          <p>
+            "Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia menciptakan
+            pasangan-pasangan untukmu dari jenismu sendiri agar kamu cenderung
+            dan merasa tenteram kepadanya, dan Dia menjadikan di antaramu rasa
+            kasih dan sayang. Sesungguhnya pada yang demikian itu benar-benar
+            terdapat tanda-tanda bagi kaum yang berpikir."
+          </p>
+        </div>
+      </div>
+      <div class="mempelai">
+        <img
+          src="Assets/images/bismillah.png"
+          alt="Bismillah"
+          class="appear"
+        />
+        <p class="appear">Assalamu'alaikum Warahmatullaahi Wabarakaatuh</p>
+        <p class="desk appear">
+          Maha Suci Allah yang telah menciptakan makhluk-Nya berpasang-pasangan.
+          Ya Allah semoga ridho-Mu tercurah mengiringi pernikahan kami.
+        </p>
+        <div class="pengantin-pria appear">
+
+          <h1>Marta</h1>
+          <h3>Marta Arofa Dilla, S.Farm.</h3>
+          <p>
+            Putri Dari <br />
+            (Alm)Bapak H Muslimin dan <br />
+            Dan (Almh) Ibu Hj Munthofiah
+          </p>
+          <a
+            href="https://www.instagram.com/martaarofadilla/?utm_source=ig_web_button_share_sheet"
+            target="_blank"
+            class="ig"
+          >
+
+            <img
+              src="Assets/icon/instagram.svg"
+              alt="instagram"
+              class="instagram"
+            />
+            Instagram
+          </a>
+        </div>
+        <div class="dan appear">---- & ----</div>
+        <div class="pengantin-wanita appear">
+
+          <h1>Rian</h1>
+          <h3>Muhammad Dzikirriansyah, S.Ak.</h3>
+          <p>
+            Putra Dari <br />
+            Bapak Ruly Rismansyah <br />
+            Dan Ibu Delistiana
+          </p>
+          <a
+            href="https://www.instagram.com/muhammaddzikirriansyah?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+            target="_blank"
+            class="ig"
+          >
+            <img
+              src="Assets/icon/instagram.svg"
+              alt="instagram"
+              class="instagram"
+            />
+            Instagram
+          </a>
+        </div>
+      </div>
+      <div class="acara">
+        <div class="akad appear">
+          <p class="judul">Akad Nikah</p>
+          <div class="tanggal">
+            <p class="angka">28</p>
+            <p class="teks">
+              Minggu <br />
+              September <br />
+              2025
+            </p>
+          </div>
+          <p class="jam">Pukul : 10:00 - 11.00 WIB</p>
+          <p class="garis">------------------</p>
+          <p class="Lokasi">
+            Lokasi Acara : <br />
+            Perumahan Pesona Citayam Blok B03 no.02 Susukan Bojonggede Bogor
+          </p>
+        </div>
+        <div class="resepsi appear">
+          <p class="judul">Resepsi Pernikahan</p>
+          <div class="tanggal">
+            <p class="angka">28</p>
+            <p class="teks">
+              Minggu <br />
+              September <br />
+              2025
+            </p>
+          </div>
+            <p class="jam">Pukul : 11:00 - 15.00 WIB</p>
+          <p class="garis">------------------</p>
+          <p class="Lokasi">
+            Lokasi Acara : <br />
+            Perumahan Pesona Citayam Blok B03 no.02 Susukan Bojonggede Bogor
+          </p>
+        </div>
+        <div class="countdown appear">
+          <h1>Hitung Mundur</h1>
+          <div class="flipdown-container">
+            <div
+              id="flipdown"
+              class="flipdown"
+              style="text-align: center"
+            ></div>
+          </div>
+        </div>
+      </div>
+      <div class="lokasi">
+        <h1 class="appear">Lokasi Acara</h1>
+        <p class="alamat appear">
+          Perumahan Pesona Citayam Blok B03 no.02 Susukan Bojonggede Bogor
+        </p>
+        <div class="embed-map-fixed">
+          <div class="embed-map-container">
+            <iframe class="embed-map-frame" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=400&height=200&hl=en&q=Jl.%20Alamanda%20Blok%20B3%20No.2%2C%20Susukan%2C%20Kecamatan%20Bojonggede%2C%20Kabupaten%20Bogor%2C%20Jawa%20Barat%2016920&t=h&z=14&ie=UTF8&iwloc=B&output=embed">
+              </iframe></div>
+        <style>.embed-map-fixed{position:relative;text-align:right;width:400px;height:200px;}.embed-map-container{overflow:hidden;background:none!important;width:400px;height:200px;}.embed-map-frame{width:400px!important;height:200px!important;}
+        </style>
+        </div>
+        
+        <a
+          href="https://maps.app.goo.gl/1oPqtuML6eKDMvn86"
+          target="_blank"
+          class="lihatPeta appear"
+          >Lihat Lokasi Acara</a
+        >
+      </div>
+       
+      </div>
+      <div class="rsvp">
+        <h1 class="appear">RSVP & Ucapan</h1>
+        <form id="rsvpForm" class="appear">
+          <label for="name" style="font-size: 20px">Nama:</label>
+          <input type="text" id="name" name="name" required />
+
+          <div>
+            <p class="confirm">Konfirmasi Kehadiran:</p>
+
+            <div class="radio-box">
+              <div class="radio-container">
+                <input
+                  type="radio"
+                  id="yes"
+                  name="attendance"
+                  value="Hadir"
+                  required
+                />
+                <label for="yes">Hadir</label>
+              </div>
+
+              <div class="radio-container">
+                <input
+                  type="radio"
+                  id="no"
+                  name="attendance"
+                  value="Tidak Hadir"
+                  required
+                />
+                <label for="no">Tidak Hadir</label>
+              </div>
+            </div>
+          </div>
+
+          <label for="message" style="font-size: 20px">Ucapan/Doa:</label>
+          <textarea id="message" name="message" rows="4" required></textarea>
+
+          <button type="submit">Kirim</button>
+
+          <h2>Daftar Kehadiran & Ucapan</h2>
+          <div id="submittedData"></div>
+        </form>
+      </div>
+      <div class="hadiah">
+        <h1 class="appear">-- Hadiah Pernikahan --</h1>
+        <p class="appear">
+          Doa Restu Anda merupakan karunia yang sangat berarti bagi kami. Dan
+          jika memberi adalah ungkapan tanda kasih Anda, Anda dapat memberi kado
+          secara cashless.
+        </p>
+        <div class="card appear">
+          <div class="bank">
+            <img src="Assets/icon/SeaBank_logo.png" alt="bri" />
+            <p>
+              11211211 <br />
+              A/N: Marta Arofa Dilla
+            </p>
+          </div>
+          <div class="atau">-------- Atau Scan --------</div>
+          <img src="Assets/icon/qris.png" alt="qris" style="width: 80%" />
+        </div>
+        
+        <div class="card appear">
+          <div class="bank">
+            <img src="Assets/icon/Bank_Mandiri_logo.png" alt="bri" />
+            <p>
+              11211211 <br />
+              A/N: Muhammad Dzikirriansyah
+            </p>
+          </div>
+          <div class="atau">-------- Atau Scan --------</div>
+          <img src="Assets/icon/qris.png" alt="qris" style="width: 80%" />
+        </div>
+      </div>
+
+      <div class="penutup" id="penutup">
+        <img
+          src="Assets/images/Bunga-panjang.png"
+          alt="pengantin"
+          class="pengantin"
+        />
+        <p class="gowunbatang-bold">
+          Terima kasih atas perhatian dan doa restu Anda, yang menjadi kebahagiaan serta kehormatan besar bagi kami.
+        </p>
+        <div class="arabpenutup appear">
+        <p class="arabpenutup">
+        اَلْحَمْدُ لِلَّٰهِ جَزَاكُمُ اللَّٰهُ خَيْرًا</p>
+</div>
+        <p>Wassalamualaikum Warahmatullahi Wabarakatuh</p>
+        <p class="garis">--------------</p>
+        <p class="gowunbatang-bold">Turut berbahagia, segenap keluarga besar</p>
+        <h1>Marta & Rian</h1>
+        <p>Build with<img src="Assets/icon/Heart-Symbol.png" alt="icon" class="icon">Tluvly</p>
+            </div>
+    </section>
+  </body>
